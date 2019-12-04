444b91f0d858b0dea16e329e18ffd15f0cda4dbd
BrianWu <brianwu291@gmail.com>
2019-12-05 02:37:40 +0800

    refs #master, remove rm -rf command on build script

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const path = require('path')
 const port = process.env.PORT || 3000
 const app = express()
 
-app.use(express.static('build'))
+app.use(express.static('build', {
+  setHeaders: function (res) {
+    res.set('x-timestamp', Date.now())
+  }
+}))
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
