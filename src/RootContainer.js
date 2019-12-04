0f6c913ff848e45fb457c8ba0ad5b815fedd093d
BrianWu <brianwu291@gmail.com>
2019-12-05 01:49:20 +0800

    refs #hotfix0002, find out that mac has lower case and upper case filename issue, fix it

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import reduxThunk from 'redux-thunk'
 import reducers from './lib/reducers'
-import ThemeContext from './themeContext'
+import ThemeContext from './ThemeContext'
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
 const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(reduxThunk)))
